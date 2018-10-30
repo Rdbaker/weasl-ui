6f04a6f19460c03f48c2fe398434a7bf6744f7c8
tyler <tylerstonephoto@gmail.com>
2018-10-29 23:05:40 -0400

    added content for pages

--- src/views/account/settings/index.js
+++ src/views/account/settings/index.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import Typography from '@material-ui/core/Typography';
+import { Form,
+         TextInput,
+         Button } from 'carbon-components-react';
+import './style.css';
 
 class AccountSettings extends Component {
   render() {
     return (
       <div>
-        Settings
+        <Typography variant={'body1'}>
+            Welcome to your settings! Here you can update information related to your Weasl profile.
+        </Typography>
+        <Form className="wsl-form">
+            <TextInput labelText="App Name"></TextInput>
+            <TextInput labelText="Login Description"></TextInput>
+            <TextInput labelText="Logout Description"></TextInput>
+            <Button type="submit">Save</Button>
+        </Form>
       </div>
     );
   }
