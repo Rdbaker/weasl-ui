93dafe6048b39aa6172eaedcc5e4e96b764d5959
Ryan Baker <ryan.da.baker@gmail.com>
2018-12-13 21:10:44 -0500

    Allows users to copy the JavaScript snippet

--- src/views/account/snippet/index.js
+++ src/views/account/snippet/index.js
@@ -1,19 +1,73 @@
 import React, { Component } from 'react';
 import Typography from '@material-ui/core/Typography';
 import { CodeSnippet } from 'carbon-components-react';
+
+import { OrgsAPI } from 'api/org';
+
 import './style.css';
 
+const getSnippet = (clientId) => (`(function(window, document) {
+if (window.weasl) { console.error('Weasl embed already included'); return; }
+var w = {}, m = ['init', 'login', 'signup', 'setAttribute', 'getCurrentUser']; w._c = [];
+m.forEach(function(me) {w[me] = function() {w._c.push([me, arguments])};});
+var elt = document.createElement('script');
+elt.type = "text/javascript"; elt.async = true;
+elt.src = "https://js.weasl.in/embed/shim.js";
+var before = document.getElementsByTagName('script')[0];
+before.parentNode.insertBefore(elt, before);
+window.weasl = w;
+})(window, document, undefined);
+weasl.init('${clientId}');
+`)
+
 class AccountSnippet extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      orgData: null,
+      clientId: null,
+    };
+  }
+
+  componentDidMount() {
+    this.doFetchMyOrg();
+  }
+
+  doFetchMyOrg = async () => {
+    const response = await OrgsAPI.getMyOrg();
+    const { data } = await response.json();
+    this.setState({
+      orgData: data,
+      clientId: data.client_id,
+    });
+  }
+
+  copySnippet = () => {
+    this.textArea.focus();
+    this.textArea.select();
+    document.execCommand('copy');
+  }
+
   render() {
-    var snippet = 'Weasl.add(plugin).go().activate();';
+    const {
+      clientId,
+    } = this.state;
+
     return (
       <div>
         <Typography variant={'body1'}>
             To add Weasl to your app, copy the snippet of code below and paste it where you would like to manage authentication. You can then use Weasl's API to authenticate users!
         </Typography>
-        <CodeSnippet type="multi" className="wsl-snippet" onClick={() => { alert("implement copy"); }}>
-            {snippet}
-        </CodeSnippet>
+        {!clientId &&
+          <div>Loading your snippet</div>
+        }
+        {clientId &&
+          <CodeSnippet className="wsl-snippet" type="multi" onClick={this.copySnippet}>
+            {getSnippet(clientId)}
+          </CodeSnippet>
+        }
+        <textarea className="wsl-snippet-textarea" ref={elt => this.textArea = elt} defaultValue={getSnippet(clientId)} />
       </div>
     );
   }
