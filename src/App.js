94dc082ab23bb3656b0c8014e56137adf3b76159
Ryan Baker <ryan.da.baker@gmail.com>
2020-01-30 09:34:50 -0500

    Removes Drift

--- src/App.js
+++ src/App.js
@@ -70,9 +70,6 @@ class App extends Component {
     this.mountWeasl();
     global.weasl.onload = this.makeWeaslOnloadFunc();
     global.weasl.onEmailVerify = this.makeWeaslOnEmailVerifyFunc();
-    if (!DEBUG) {
-      this.mountDrift();
-    }
   }
 
   mountWeasl = () => {
@@ -88,31 +85,6 @@ class App extends Component {
     global.weasl.init(WEASL_ON_WEASL_CLIENT_ID);
   }
 
-  mountDrift = () => {
-    !function() {
-      var t = window.driftt = window.drift = window.driftt || [];
-      if (!t.init) {
-        if (t.invoked) return void (window.console && console.error && console.error("Drift snippet included twice."));
-        t.invoked = !0, t.methods = [ "identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on" ],
-        t.factory = function(e) {
-          return function() {
-            var n = Array.prototype.slice.call(arguments);
-            return n.unshift(e), t.push(n), t;
-          };
-        }, t.methods.forEach(function(e) {
-          t[e] = t.factory(e);
-        }), t.load = function(t) {
-          var e = 3e5, n = Math.ceil(new Date() / e) * e, o = document.createElement("script");
-          o.type = "text/javascript", o.async = !0, o.crossorigin = "anonymous", o.src = "https://js.driftt.com/include/" + n + "/" + t + ".js";
-          var i = document.getElementsByTagName("script")[0];
-          i.parentNode.insertBefore(o, i);
-        };
-      }
-    }();
-    drift.SNIPPET_VERSION = '0.3.1';
-    drift.load('2b7p8yk2aw3x');
-  }
-
   makeLoginRequiredComponent(AuthedComponent) {
     const {
       isLoggedIn,
@@ -122,10 +94,6 @@ class App extends Component {
 
     return () => {
       if (isLoggedIn) {
-        global.drift && global.drift.on('ready', () => {
-          drift.identify(currentUser.id, { ...currentUser });
-        });
-
         global.Sentry && global.Sentry.configureScope && global.Sentry.configureScope((scope) => {
           scope.setUser(currentUser);
         });
